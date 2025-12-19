17238cce0ee901fa673dfee1502ea896bc520254
raheelgudeman <raheelgudeman@gmail.com>
2025-12-19 12:17:37 -0500

    Create python_basics.py

---
+++ basics/python_basics.py
@@ -0,0 +1,16 @@
+# Python Basics for Defense Analytics
+# Author: Raheel Gudeman
+
+# --- Basic Variables ---
+unit_name = "Logistics Platoon"
+total_vehicles = 24
+vehicles_operational = 18
+
+# --- Simple Calculation ---
+readiness_rate = (vehicles_operational / total_vehicles) * 100
+
+# --- Output ---
+print("Unit:", unit_name)
+print("Total Vehicles:", total_vehicles)
+print("Operational Vehicles:", vehicles_operational)
+print("Readiness Rate:", readiness_rate, "%")
